ca8e2c76cfadb0b30fff5e6a6ad3c3dcafcf37da
Fezzopro <fezzopro@gmail.com>
2023-09-13 22:09:42 +0200

    Add group validation specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Groups velidation specs' do
+    it { should validate_presence_of(:name) }
+    it { should belong_to(:user) }
+    it { should have_db_column(:icon) }
+    it { should have_db_column(:name) }
+  end
 end
