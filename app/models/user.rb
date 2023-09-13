7d7c8f67a4cc1990978ed2c1d6bed80e19d3595c
Fezzopro <fezzopro@gmail.com>
2023-09-13 15:48:24 +0200

    Add User validations and user -validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :entities
   has_many :groups
+
+  validates :name, presence: true, uniqueness: true
+  validates_length_of :name, within: 3..20
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'User validation' do
+    it { should validate_presence_of(:name) }
+    it { should have_many(:groups) }
+    it { should have_many(:entities) }
+    it { should validate_length_of(:name).is_at_least(3).on(:create) }
+    it { should validate_length_of(:name).is_at_most(20).on(:create) }
+    it { should validate_uniqueness_of(:name) }
+  end
 end
