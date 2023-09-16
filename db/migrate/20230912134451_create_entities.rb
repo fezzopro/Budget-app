71d134d1b05a83da1b4f8f1b084d59e83fb73369
Fezzopro <fezzopro@gmail.com>
2023-09-16 19:41:12 +0200

    Add migrations

--- db/migrate/20230912134451_create_entities.rb
+++ db/migrate/20230912134451_create_entities.rb
@@ -2,7 +2,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
       t.string :name
-      t.float :amount
+      t.decimal :amount
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
