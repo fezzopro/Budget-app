0d4f8bd266d78b7df48b35e9d468d5ba94428baa
Fezzopro <fezzopro@gmail.com>
2023-09-16 19:45:15 +0200

    Add RESTful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # get 'category/index'
-  # get 'category/new'
-  # get 'category/edit'
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  
   root 'home#index'
-  devise_for :users
-  # get 'user/index'
-  resources :category, only: [:index, :new, :edit, :update, :show, :destory]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :category
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :entities, only: [:new, :create]
+  end
 end
